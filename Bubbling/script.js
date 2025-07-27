b0a107a32e1998b6e1307fc9127fdebcba34c8ac
Cyber-Cp <hellohavaki2@gmail.com>
2025-07-27 10:11:32 +0530

    This is Bubblin

---
+++ Bubbling/script.js
@@ -0,0 +1,15 @@
+document.getElementById('div1').addEventListener('click',()=>{
+    alert("Div1");
+});
+document.getElementById('div2').addEventListener('click',()=>{
+    alert('Div2')
+});
+document.getElementById('div3').addEventListener('click',()=>{
+    alert("Div3")
+});
+document.getElementById('div4').addEventListener('click',() =>{
+    alert("Div4")
+})
+document.getElementById('div5').addEventListener('click',()=>{
+    alert('Div 5')
+})
